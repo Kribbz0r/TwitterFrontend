bb886bef32396ef40a1b3ec22fd74a83169c9eff
Kribbz0r <kribbz0r@gmail.com>
2025-06-23 18:02:19 +0200

    adds working input fields

--- src/features/register/components/registerModal/forms/FormOne.tsx
+++ src/features/register/components/registerModal/forms/FormOne.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { TextInput } from '../../../../../components/modal/textInput/TextInput'
 import './formOne.css'
+
+interface FormOneState {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dateOfBirth: string;
+}
 
 
 export const FormOne: React.FC = () => {
+
+    const [stepOneState, setStepOneState] = useState<FormOneState>({
+        firstName: "",
+        lastName: "",
+        email: "",
+        dateOfBirth: ""
+    });
+
+
+    const updateUser = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setStepOneState({ ...stepOneState, [e.target.name]: e.target.value });
+    }
+
+    useEffect(() => {
+        console.log("New State: ", stepOneState);
+    }, [stepOneState])
+
     return (
         <div className='registerStepOneContainer'>
             <div className='registerStepOneContent'>
-                Input area
+                <TextInput name={"firstName"} label={'First'} errorMessage={'Enter your name, please'} onChange={updateUser} />
+                <TextInput name={"lastName"} label={'Last'} errorMessage={'Enter your surname, please'} onChange={updateUser} />
+                <TextInput name={"email"} label={'Email'} errorMessage={'Enter your email, please'} onChange={updateUser} />
             </div>
 
         </div>
