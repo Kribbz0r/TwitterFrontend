8b3e7b785ad426f831d1c4b62a45b31f277b73dc
Kribbz0r <kribbz0r@gmail.com>
2025-06-25 18:21:13 +0200

    adds styling to labels and borders

--- src/components/validInput/ValidInput.tsx
+++ src/components/validInput/ValidInput.tsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
 import { StyledInputProps } from "../../features/utils/GlogalInterfaces";
-import { determinStyledInputBorder } from "../../features/utils/DeterminStyleUtils";
+import { determineLabelColor, determinStyledInputBorder } from "../../features/utils/DeterminStyleUtils";
 
 export const StyledInputBox = styled.div<StyledInputProps>`
 
 position : relative;
 border-radius: 5px;
-width : 100%
-height :56px;
+width : 100%;
+height : 56px;
 border : ${(props) => determinStyledInputBorder(props)};
+`;
+
+export const StyledInputLabel = styled.span<StyledInputProps>`
+position :absolute;
+left:10px;
+font-weight : 400;
+width: 100%;
+margin:0;
+padding: 0;
+font-size : ${(props) => props.active ? '12px' : '20px'};
+top : ${(props) => props.active ? '5px' : '17px'};
+color: ${(props) => determineLabelColor(props)}
 `;
--- src/features/utils/DeterminStyleUtils.tsx
+++ src/features/utils/DeterminStyleUtils.tsx
@@ -1,6 +1,42 @@
+import { act } from "react";
 import { StyledInputBox } from "../../components/validInput/ValidInput";
 import { StyledInputProps } from "./GlogalInterfaces";
 
 export const determinStyledInputBorder = (props: StyledInputProps): string => {
+
+    let { active, valid, theme } = props;
+
+    if (!active && valid) {
+        return `1px solid ${theme.colors.lightGray}`;
+    }
+
+    if (!active && !valid) {
+        return `1px solid ${theme.colors.error} `;
+    }
+
+    if (active && valid) {
+        `2px solid ${theme.colors.blue}`
+    }
+
+    if (active && !valid) {
+        return `2px solid ${theme.colors.error} `;
+    }
     return "";
+}
+
+export const determineLabelColor = (props: StyledInputProps): string => {
+
+    let { active, valid, theme, color } = props;
+    if (color && color === 'error') {
+        return theme.colors.error;
+
+    }
+
+    if (color && color === 'blue') {
+        return theme.colors.blue;
+
+    }
+
+
+    return theme.colors.lightGray;
 }
